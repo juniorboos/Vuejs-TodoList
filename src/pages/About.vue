aa8687f062015d08d4fe5a5418263ef916cdc0f5
Milton Boos Junior <junior_boos@live.com>
2020-11-17 19:24:37 +0000

    final project

---
+++ src/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is made by referencing the Vue crash course on Youtube</p>
+  </div>
+</template>
+
+<style scoped>
+   h1, p {
+      margin-top: 18px;
+      text-align: center;
+   }  
+</style>
